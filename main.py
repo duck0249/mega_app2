94106823e3b1b8b914107441748bda5432a5665c
duck0249 <donggeunlee@Donggeuns-MacBook-Air.local>
2024-10-07 08:50:46 +0700

    Adding my image and base code update of main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# App2 - Python mega course
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+	st.image("images/Lee_Dong_Geun.png")
+
+with col2:
+	st.title("Andrew Lee")
+	contents = """
+	Hello, I am Andrew Lee. I am a python developer, and managing director of Hanon Systems Thailand and South Africa. 
+	I was on board in Thailand in 2021 and I used to work as planning director in Seoul, Korea.
+	Currently, I am in the live project of office automation powered by Python, MySQL and PowerBI and
+	this project due is the end of 2024. It will give me a lot of skillsets and improve my operation efficiencies significantly.
+	All of apps are dedicated to my project purpose either actual project or training.
+	"""
+	st.info(contents)
